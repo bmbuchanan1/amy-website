a19ae815ac5c00f6fc315445e817707bf281b49c
bbuchanan1 <77042437+bmbuchanan1@users.noreply.github.com>
2026-09-06 17:03:45 +0200

    assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: '/amy-website'
+  basePath: "/amy-website",
+  assetPrefix: "/amy-website"
 
 };
 

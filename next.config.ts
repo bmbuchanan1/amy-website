6d0d1dbcde6be7fe275f0e89a6162c2600bdc74b
bbuchanan1 <77042437+bmbuchanan1@users.noreply.github.com>
2026-09-06 16:57:28 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  
+  basePath: '/amy-website'
+
 };
 
 export default nextConfig;

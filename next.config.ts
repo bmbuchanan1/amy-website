221f65c6df28c5ee90f1e24fa697eebc0d16c578
bbuchanan1 <77042437+bmbuchanan1@users.noreply.github.com>
2026-09-06 17:11:37 +0200

    assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'your-repo-name'; // Replace with your exact GitHub repository name
+const repoName = 'amy-website'; // Replace with your exact GitHub repository name
 
 const nextConfig: NextConfig = {
   output: "export",
